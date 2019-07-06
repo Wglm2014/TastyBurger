ccdd5ef2b486889547341609d1107138bd95bb96
Wglm2014 <glinaresmiranda@yahoo.com>
2019-07-06 16:19:46 -0400

    correction of connection values in config

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const expresshb = require("express-handlebars");
 const PORT = process.env.PORT || 8080;
 const app = express();
+const path = require("path");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
