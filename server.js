537b6f3560dc5e3d8f25cd094acf614c9a4b860f
Wglm2014 <glinaresmiranda@yahoo.com>
2019-07-06 13:29:09 -0400

    modification of config and package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expresshb = require("express-handlebars");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static("public"));
